88f7c2ed1698256ee82610ad4c09fe725ded9dc7
shihabmi7 <shihab.mi7@gmail.com>
2019-09-02 20:52:59 +0600

    add with error

--- app/src/main/java/com/shihab/kotlintoday/DataTypeActivity.kt
+++ app/src/main/java/com/shihab/kotlintoday/DataTypeActivity.kt
@@ -8,15 +8,35 @@ import kotlinx.android.synthetic.main.content_data_type.*
 
 class DataTypeActivity : AppCompatActivity() {
 
+    var age = 30
+    val year: Int = 2000 // It can change ()
+
+    val evenNumber = "Number is Even"
+    val oddNumber = "Number is Odd"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_type)
         setSupportActionBar(toolbar)
 
-        var toast = Toast.makeText(applicationContext, "", Toast.LENGTH_LONG)
-
         button_show.setOnClickListener {
             Toast.makeText(applicationContext, edit_name.text.toString(), Toast.LENGTH_LONG).show()
+
+        }
+
+        button_check_condition.setOnClickListener {
+
+
+            var value = edit_odd_even.text
+
+            if (value % 2 == 0) {
+                Toast.makeText(applicationContext, evenNumber, Toast.LENGTH_LONG).show()
+
+            } else {
+                Toast.makeText(applicationContext, oddNumber, Toast.LENGTH_LONG).show()
+
+            }
+
         }
     }
 
