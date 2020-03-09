03c4f4dfa28e3b31b3c92e7c3605629d9dac930e
shihab_07 <shihab@sharetrip.net>
2020-03-09 17:50:08 +0600

    view binding

--- app/src/main/java/com/shihab/kotlintoday/feature/viewBinding/ViewBindingActivity.kt
+++ app/src/main/java/com/shihab/kotlintoday/feature/viewBinding/ViewBindingActivity.kt
@@ -3,6 +3,7 @@ package com.shihab.kotlintoday.feature.viewBinding
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.shihab.kotlintoday.databinding.ActivityViewBindingBinding
+import kotlinx.android.synthetic.main.activity_view_binding.*
 import kotlinx.android.synthetic.main.content_view_binding.*
 
 class ViewBindingActivity : AppCompatActivity() {
@@ -14,5 +15,7 @@ class ViewBindingActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         textview_test.text = "Hello world in view Binding...!!!"
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "view Binding"
     }
 }
