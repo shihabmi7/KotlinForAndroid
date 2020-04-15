bf6abd5ff740fa49db26053d3f6658535c74eb69
shihab_returns <shihab.apps.developer@gmail.com>
2020-04-15 20:09:12 +0600

    update todo

--- app/src/main/java/com/shihab/kotlintoday/utility/MyTodo.kt
+++ app/src/main/java/com/shihab/kotlintoday/utility/MyTodo.kt
@@ -1,5 +1,13 @@
 package com.shihab.kotlintoday.utility
 
+// mvvm swipe delete animation, edit notes
+// Dagger with MVVB
+// Pagging
+// type script
+// circular testing
+// testing..Unit test
+// mockito.
+
 
 // Recycler
 //https://medium.com/better-programming/android-recyclerview-with-beautiful-animations-5e9b34dbb0fa
