c7516df1ecdf81e628cf58642e724daf721fd925
shihab_07 <shihab@sharetrip.net>
2020-08-19 16:48:58 +0600

    successfully used firebase test lab

---
+++ app/src/androidTest/java/com/shihab/kotlintoday/feature/NavigationSuites.kt
@@ -0,0 +1,12 @@
+package com.shihab.kotlintoday.feature
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    HomeActivityTest::class
+)
+class NavigationSuites
+
+/** we can run this */
