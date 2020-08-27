f8a2a0f6cc629a10cd83b3a057785e32fc4d4d57
shihab_07 <shihab@sharetrip.net>
2020-08-27 18:53:57 +0600

    Successfully apple sign in implemented

--- app/src/main/java/com/shihab/kotlintoday/feature/apple_sign_in/AppleConstants.kt
+++ app/src/main/java/com/shihab/kotlintoday/feature/apple_sign_in/AppleConstants.kt
@@ -2,7 +2,8 @@ package com.shihab.kotlintoday.feature.apple_sign_in
 
 object AppleConstants {
     val CLIENT_ID = "net.sharetrip.android"
-    val REDIRECT_URI = "https://sharetrip.net"
+    val BEFORE_REDIRECT_URI = "https://xyz.net/api/v1/auth/apple-token"
+    val REDIRECT_URI = "https://xyz.net?appleToken"
     val SCOPE = "name%20email"
     val RESPONSE_TYPE = "code%20id_token"
     val RESPONSE_MODE = "form_post"
@@ -10,7 +11,8 @@ object AppleConstants {
     val AUTHURL = "https://appleid.apple.com/auth/authorize"
     val TOKENURL = "https://appleid.apple.com/auth/token"
 
-    //https://appleid.apple.com/auth/authorize?client_id=net.sharetrip.android&
-    // redirect_uri=https://sharetrip.net&response_type=code%20id_token&
-    // scope=name%20email&response_mode=form_post&state
+//    https://appleid.apple.com/auth/
+//    authorize?response_type=code%20id_token&response_mode=form_post&client_id=net.sharetrip.android
+//    &scope=name%20email&state=fe6e75b4-3fb8-4946-8717-883b9c44b768&redirect_uri=https://stg-api.sharetrip.net/api/v1/auth/apple-token
+
 }
--- app/src/main/java/com/shihab/kotlintoday/feature/apple_sign_in/AppleSignInActivity.kt
+++ app/src/main/java/com/shihab/kotlintoday/feature/apple_sign_in/AppleSignInActivity.kt
@@ -1,19 +1,13 @@
 package com.shihab.kotlintoday.feature.apple_sign_in
 
-import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.app.Dialog
 import android.content.Context
-import android.graphics.Rect
+import android.net.ConnectivityManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
+import android.view.KeyEvent
 import android.view.View
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -22,31 +16,17 @@ import androidx.databinding.DataBindingUtil
 import com.shihab.kotlintoday.R
 import com.shihab.kotlintoday.databinding.ActivityAppleSignInBinding
 import com.shihab.kotlintoday.utility.LogMe
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import org.json.JSONObject
-import org.json.JSONTokener
-import java.io.OutputStreamWriter
-import java.net.URL
 import java.util.*
-import javax.net.ssl.HttpsURLConnection
 
 class AppleSignInActivity : AppCompatActivity() {
 
+    private val APPLE_TOKEN_KEY: String ="appleToken"
     lateinit var binding: ActivityAppleSignInBinding
     lateinit var appleAuthURLFull: String
     lateinit var appledialog: Dialog
-    lateinit var appleAuthCode: String
-    lateinit var appleClientSecret: String
+    private var webViewBundle: Bundle? = null
 
-    var appleId = ""
-    var appleFirstName = ""
-    var appleMiddleName = ""
-    var appleLastName = ""
-    var appleEmail = ""
-    var appleAccessToken = ""
+    private var appleAccessToken = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,7 +36,7 @@ class AppleSignInActivity : AppCompatActivity() {
         val state = UUID.randomUUID().toString()
 
         appleAuthURLFull =
-            AppleConstants.AUTHURL + "?response_type=" + AppleConstants.RESPONSE_TYPE + "&response_mode=" + AppleConstants.RESPONSE_MODE + "&client_id=" + AppleConstants.CLIENT_ID + "&scope=" + AppleConstants.SCOPE + "&state=" + state + "&redirect_uri=" + AppleConstants.REDIRECT_URI
+            AppleConstants.AUTHURL + "?response_type=" + AppleConstants.RESPONSE_TYPE + "&response_mode=" + AppleConstants.RESPONSE_MODE + "&client_id=" + AppleConstants.CLIENT_ID + "&scope=" + AppleConstants.SCOPE + "&state=" + state + "&redirect_uri=" + AppleConstants.BEFORE_REDIRECT_URI
 
         LogMe.i("url",appleAuthURLFull)
         binding.content.relativeLayout.setOnClickListener {
@@ -66,37 +46,31 @@ class AppleSignInActivity : AppCompatActivity() {
 
     private fun setupAppleWebviewDialog(url: String) {
         appledialog = Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen)
-        appledialog.setContentView(R.layout.fragment_payment_method)
+        appledialog.setContentView(R.layout.fragment_apple_sign_in)
         val layout = appledialog.findViewById<LinearLayout>(R.id.layoutProgress)
         val progressBar = appledialog.findViewById<ProgressBar>(R.id.progressBar)
         val webView = appledialog.findViewById<WebView>(R.id.webView)
-        webView.isVerticalScrollBarEnabled = true
-        webView.isHorizontalScrollBarEnabled = false
-        webView.webViewClient = object : WebViewClient() {
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            override fun shouldOverrideUrlLoading(
-                view: WebView?,
-                request: WebResourceRequest?
-            ): Boolean {
-                if (request!!.url.toString().startsWith(AppleConstants.REDIRECT_URI)) {
-                    handleUrl(request.url.toString())
-                    // Close the dialog after getting the authorization code
-                    if (request.url.toString().contains("success=")) {
-                        appledialog.dismiss()
-                    }
-                    return true
-                }
-                return true
-            }
 
-            // For API 19 and below
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.builtInZoomControls = true
+        settings.setSupportZoom(true)
+        settings.displayZoomControls = false
+
+
+        webView.settings.userAgentString = "Android_Tbbd"
+        webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+
+        webView.webViewClient = object : WebViewClient() {
+
             override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-                if (url.startsWith(AppleConstants.REDIRECT_URI)) {
-                    handleUrl(url)
-                    // Close the dialog after getting the authorization code
-                    if (url.contains("success=")) {
-                        appledialog.dismiss()
-                    }
+                if (url.contains("appleToken=",false)){
+                    LogMe.i("ri-url",url)
+                    handleURL(url)
+                    appledialog.dismiss()
+
                     return true
                 }
                 return false
@@ -108,222 +82,53 @@ class AppleSignInActivity : AppCompatActivity() {
                 progressBar.isIndeterminate = false
                 super.onPageFinished(view, url)
             }
+        }
 
-            // Check webview url for access token code or error
-            private fun handleUrl(url: String) {
-                val uri = Uri.parse(url)
-
-                val success = uri.getQueryParameter("success")
-                if (success == "true") {
-
-                    // Get the Authorization Code from the URL
-                    appleAuthCode = uri.getQueryParameter("code") ?: ""
-                    Log.i("Apple Code: ", appleAuthCode)
-
-                    // Get the Client Secret from the URL
-                    appleClientSecret = uri.getQueryParameter("client_secret") ?: ""
-                    Log.i("Apple Client Secret: ", appleClientSecret)
-                    // Save the Client Secret (appleClientSecret) using SharedPreferences
-                    // This will allow us to verify if refresh Token is valid every time they open the app after cold start.
-                    val sharedPref = getPreferences(Context.MODE_PRIVATE)
-                    sharedPref.edit().putString("client_secret", appleClientSecret).apply()
-
-                    //Check if user gave access to the app for the first time by checking if the url contains their email
-                    if (url.contains("email")) {
-                        //Get user's First Name
-                        val firstName = uri.getQueryParameter("first_name")
-                        Log.i("Apple User First Name: ", firstName ?: "")
-                        appleFirstName = firstName ?: "Not exists"
-
-                        //Get user's Middle Name
-                        val middleName = uri.getQueryParameter("middle_name")
-                        Log.i("Middle Name: ", middleName ?: "")
-                        appleMiddleName = middleName ?: "Not exists"
-
-                        //Get user's Last Name
-                        val lastName = uri.getQueryParameter("last_name")
-                        Log.i("Apple User Last Name: ", lastName ?: "")
-                        appleLastName = lastName ?: "Not exists"
-
-                        //Get user's email
-                        val email = uri.getQueryParameter("email")
-                        Log.i("Apple User Email: ", email ?: "Not exists")
-                        appleEmail = email ?: ""
-                    }
-
-                    // Exchange the Auth Code for Access Token
-                    requestForAccessToken(appleAuthCode, appleClientSecret)
-                } else if (success == "false") {
-                    Log.e("ERROR", "We couldn't get the Auth Code")
-                }
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
+                return true
             }
         }
-        webView.settings.javaScriptEnabled = true
-        webView.loadUrl(url)
+
+        webView.setOnKeyListener( View.OnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                val webView = v as WebView
+
+                when (keyCode) {
+                    KeyEvent.KEYCODE_BACK -> if (webView.canGoBack()) {
+                        webView.goBack()
+                        return@OnKeyListener true
+                    }
+                }
+            }
+            false
+        })
+
+        if (isOnline()) {
+            if (webViewBundle == null) {
+                webView.loadUrl(url)
+            } else {
+                webView.restoreState(webViewBundle)
+            }
+        } else {
+            val summary = "<html><body><font color='red'>No Internet Connection</font></body></html>"
+            webView.loadData(summary, "text/html", null)
+            Toast.makeText(this, "No Internet Connection.", Toast.LENGTH_SHORT).show()
+        }
+
         appledialog.show()
     }
 
-    /*@Suppress("OverridingDeprecatedMember")
-    inner class AppleWebViewClient : WebViewClient() {
-        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-        override fun shouldOverrideUrlLoading(
-            view: WebView?,
-            request: WebResourceRequest?
-        ): Boolean {
-            if (request!!.url.toString().startsWith(AppleConstants.REDIRECT_URI)) {
-                handleUrl(request.url.toString())
-                // Close the dialog after getting the authorization code
-                if (request.url.toString().contains("success=")) {
-                    appledialog.dismiss()
-                }
-                return true
-            }
-            return true
-        }
+    private fun handleURL(url: String) {
+        val uri = Uri.parse(url)
 
-        // For API 19 and below
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            if (url.startsWith(AppleConstants.REDIRECT_URI)) {
-                handleUrl(url)
-                // Close the dialog after getting the authorization code
-                if (url.contains("success=")) {
-                    appledialog.dismiss()
-                }
-                return true
-            }
-            return false
-        }
-
-        @SuppressLint("ClickableViewAccessibility")
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-            // retrieve display dimensions
-            val displayRectangle = Rect()
-            val window = this@AppleSignInActivity.window
-            window.decorView.getWindowVisibleDisplayFrame(displayRectangle)
-
-            // Set height of the Dialog to 90% of the screen
-            val layoutparms = view?.layoutParams
-            layoutparms?.height = (displayRectangle.height() * 0.9f).toInt()
-            view?.layoutParams = layoutparms
-        }
-
-        // Check webview url for access token code or error
-        @SuppressLint("LongLogTag")
-        private fun handleUrl(url: String) {
-            val uri = Uri.parse(url)
-
-            val success = uri.getQueryParameter("success")
-            if (success == "true") {
-
-                // Get the Authorization Code from the URL
-                appleAuthCode = uri.getQueryParameter("code") ?: ""
-                Log.i("Apple Code: ", appleAuthCode)
-
-                // Get the Client Secret from the URL
-                appleClientSecret = uri.getQueryParameter("client_secret") ?: ""
-                Log.i("Apple Client Secret: ", appleClientSecret)
-                // Save the Client Secret (appleClientSecret) using SharedPreferences
-                // This will allow us to verify if refresh Token is valid every time they open the app after cold start.
-                val sharedPref = getPreferences(Context.MODE_PRIVATE)
-                sharedPref.edit().putString("client_secret", appleClientSecret).apply()
-
-                //Check if user gave access to the app for the first time by checking if the url contains their email
-                if (url.contains("email")) {
-                    //Get user's First Name
-                    val firstName = uri.getQueryParameter("first_name")
-                    Log.i("Apple User First Name: ", firstName ?: "")
-                    appleFirstName = firstName ?: "Not exists"
-
-                    //Get user's Middle Name
-                    val middleName = uri.getQueryParameter("middle_name")
-                    Log.i("Apple User Middle Name: ", middleName ?: "")
-                    appleMiddleName = middleName ?: "Not exists"
-
-                    //Get user's Last Name
-                    val lastName = uri.getQueryParameter("last_name")
-                    Log.i("Apple User Last Name: ", lastName ?: "")
-                    appleLastName = lastName ?: "Not exists"
-
-                    //Get user's email
-                    val email = uri.getQueryParameter("email")
-                    Log.i("Apple User Email: ", email ?: "Not exists")
-                    appleEmail = email ?: ""
-                }
-
-                // Exchange the Auth Code for Access Token
-                requestForAccessToken(appleAuthCode, appleClientSecret)
-            } else if (success == "false") {
-                Log.e("ERROR", "We couldn't get the Auth Code")
-            }
-        }
-
-    }*/
-
-    private fun requestForAccessToken(code: String, clientSecret: String) {
-        val grantType = "authorization_code"
-
-        val postParamsForAuth =
-            "grant_type=" + grantType + "&code=" + code + "&redirect_uri=" + AppleConstants.REDIRECT_URI + "&client_id=" + AppleConstants.CLIENT_ID + "&client_secret=" + clientSecret
-
-
-        GlobalScope.launch {
-            val url = URL(AppleConstants.TOKENURL)
-            val httpsURLConnection =
-                withContext(Dispatchers.IO) { url.openConnection() as HttpsURLConnection }
-            httpsURLConnection.requestMethod = "POST"
-            httpsURLConnection.setRequestProperty(
-                "Content-Type",
-                "application/x-www-form-urlencoded"
-            )
-            httpsURLConnection.doInput = true
-            httpsURLConnection.doOutput = true
-            withContext(Dispatchers.IO) {
-                val outputStreamWriter = OutputStreamWriter(httpsURLConnection.outputStream)
-                outputStreamWriter.write(postParamsForAuth)
-                outputStreamWriter.flush()
-            }
-            val response = httpsURLConnection.inputStream.bufferedReader()
-                .use { it.readText() }  // defaults to UTF-8
-
-            val jsonObject = JSONTokener(response).nextValue() as JSONObject
-
-            val accessToken = jsonObject.getString("access_token") //Here is the access token
-            Log.i("Apple Access Token is: ", accessToken)
-            appleAccessToken = accessToken
-
-            val expiresIn = jsonObject.getInt("expires_in") //When the access token expires
-            Log.i("expires in: ", expiresIn.toString())
-
-            val refreshToken =
-                jsonObject.getString("refresh_token") // The refresh token used to regenerate new access tokens. Store this token securely on your server.
-            Log.i("refresh token: ", refreshToken)
-            // Save the RefreshToken Token (refreshToken) using SharedPreferences
-            // This will allow us to verify if refresh Token is valid every time they open the app after cold start.
-            val sharedPref = getPreferences(Context.MODE_PRIVATE)
-            sharedPref.edit().putString("refresh_token", refreshToken ?: "").apply()
-
-
-            val idToken =
-                jsonObject.getString("id_token") // A JSON Web Token that contains the user’s identity information.
-            Log.i("ID Token: ", idToken)
-
-            // Get encoded user id by splitting idToken and taking the 2nd piece
-            val encodedUserID = idToken.split(".")[1]
-
-            //Decode encodedUserID to JSON
-            val decodedUserData = String(Base64.decode(encodedUserID, Base64.DEFAULT))
-            val userDataJsonObject = JSONObject(decodedUserData)
-            // Get User's ID
-            val userId = userDataJsonObject.getString("sub")
-            Log.i("Apple User ID :", userId)
-            appleId = userId
-
-            withContext(Dispatchers.Main) {
-                //
-                //openDetailsActivity()
-            }
-        }
+        appleAccessToken = uri.getQueryParameter(APPLE_TOKEN_KEY)
+        LogMe.i("appleAccessToken",appleAccessToken)
     }
 
+    private fun isOnline(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = cm.activeNetworkInfo
+        return netInfo != null && netInfo.isConnected
+    }
 }
