8943798b9602b21484e81c3897c0dff6e4fd800b
shihab_07 <shihab@sharetrip.net>
2021-01-31 10:13:56 +0600

    update home activity test

--- app/src/androidTest/java/com/shihab/kotlintoday/feature/HomeActivityTest.kt
+++ app/src/androidTest/java/com/shihab/kotlintoday/feature/HomeActivityTest.kt
@@ -1,12 +1,12 @@
 package com.shihab.kotlintoday.feature
 
 import androidx.lifecycle.Lifecycle
-import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.shihab.kotlintoday.R
@@ -30,17 +30,13 @@ class HomeActivityTest {
     @Test
     fun test_isActivityInView() {
 
-//        val activityScene = ActivityScenario.launch(HomeActivity::class.java)
-        onView(ViewMatchers.withId(R.id.layoutContent)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.layoutContent)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
         // perform a click
-        //onView(withId(R.id.refresh)).perform(click())
-
-        // Activity under test is now finished.
+        //(withId(R.id.refresh)).perform(click())
 
         /*val resultCode = scenario.result.resultCode
         val resultData = scenario.result.resultData*/
-
 
         /** To call method activity itself*/
         activityScenarioRule.scenario.onActivity { activity ->
@@ -48,20 +44,15 @@ class HomeActivityTest {
         }
 
         /** Move to a activity life cycle*/
-        //activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
+        activityScenarioRule.scenario.moveToState(Lifecycle.State.CREATED)
 
         /** back button press*/
-        //pressBack()
+        pressBack()
     }
-
 
     @Test
     fun test_checkRecyclerViewItem() {
-
-//        val activityScene = ActivityScenario.launch(HomeActivity::class.java)
-        onView(ViewMatchers.withId(R.id.recycler_content)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        //onView(Recycler).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-
+        onView(withId(R.id.recycler_content)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 
 }
