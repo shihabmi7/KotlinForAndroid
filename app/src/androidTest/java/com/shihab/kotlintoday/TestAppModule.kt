761eef2fb5d8b7ec4fc60634174e331d377c2dff
shihabmi7 <s.uddin@cheq.io>
2022-03-07 12:42:38 +0600

    Dagger Hilt- Testing part Done

--- app/src/androidTest/java/com/shihab/kotlintoday/TestAppModule.kt
+++ app/src/androidTest/java/com/shihab/kotlintoday/TestAppModule.kt
@@ -1,11 +1,22 @@
 package com.shihab.kotlintoday
 
+import android.content.Context
+import androidx.room.Room
+import com.shihab.kotlintoday.feature.mvvm.db.NoteDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
 
 @Module
 @InstallIn(SingletonComponent::class)
 object TestAppModule {
 
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryInDB(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, NoteDatabase::class.java).allowMainThreadQueries()
+            .build()
 }
--- app/src/androidTest/java/com/shihab/kotlintoday/feature/mvvm/dao/NoteDaoTestWithDagger.kt
+++ app/src/androidTest/java/com/shihab/kotlintoday/feature/mvvm/dao/NoteDaoTestWithDagger.kt
@@ -5,32 +5,41 @@ import androidx.test.filters.SmallTest
 import com.shihab.kotlintoday.feature.mvvm.db.NoteDatabase
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Named
 
+@ExperimentalCoroutinesApi
 @HiltAndroidTest
 @SmallTest
 class NoteDaoTestWithDagger {
 
-    private lateinit var database: NoteDatabase
-    private lateinit var dao: NoteDao
+    @Inject
+    @Named("test_db")
+    lateinit var database: NoteDatabase
+
+    lateinit var dao: NoteDao
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
+    @get:Rule
     var instantTaskExecutor = InstantTaskExecutorRule()
 
     @Before
     fun setup() {
-
+        hiltRule.inject()
+        dao = database.noteDao()
     }
 
     @After
     fun close() {
-
+        database.close()
     }
 
     @Test
